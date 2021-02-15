8a35f7a225cdd1f85f2e7933901d8d54653c35fb
yaturner <thursdaynext@gmail.com>
2021-02-15 10:18:50 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,130 @@
+myWeatherPanel
+============
+
+My wife is always wondering about the weather, if it going to be too hot today, too hunid, and what about the rest of the week. So I decided to create a LED panel display for her that would answer all her questions and give me an excuse to learn about LED Panels.
+
+Ok, full disclosure, the lock down was driving me nutz and I really didn't care all that much about the weather, I just needed something to do.
+
+This work is based largely on the work of others and I try to give credit where I can
+
+
+OverView
+--------
+
+This project was written in python3 on a Raspberry Pi 3B+, however almost any RPi can be used that supports WiFi. I actually run the project on a Raspberry Pi Zero WH. The project evolved as I learned more and more about using LED Panels and writing large python projects, so I am sure there are multiple places where the code can be made better, feel free to give me feedback. Here is an example of what the completed project looks like.
+
+
+![Figure 1!](/images/figure1.jpg)
+
+The left hand side shows today's temperature and humidity, the right hand side cycles through the week showing the high/low temperatures and the weather icon for each day. Below that is the wind speed and direction, and below that is a scrolling text that shows today's weather alerts, if there are any, for roughly 30 minutes then it will show to entries from the RSS feed(s) specified in the cfg file (see below).
+The application will keep alternating between weather alerts and RSS feed entries. The variable *timeOut* in the code can be tweaked to change the display period. If more that one RSS feed is specified the application will rotate through them at every time out.
+
+## Hardware
+
+
+All of these items can be ordered from Amazon, AdaFruit, or DigiKey among others
+
+- 4 pcs. 64x32 Led Panels with magnetic feet, I used a 4mm pitch (that is the distance between LEDs)
+- 2 pcs. Power supplies for the Led Panels, I used ALITOVE 5V 15Amp Power Supplies because they are specifically designed for LED Panels
+- 1 pc. Raspberry Pi that supports WiFi, I developed this on a RPi 3B+ for speed, but I run the actual project on a Rpi Zero WH
+- 1 pc. AdaFruit RGB Matrix Bonnet for Raspberry Pi
+- 1 pc. M3 Male-Female Brass Spacer Standoff & Stainless Steel Screw Nut Assortment Kit (optional)
+- 8 pcs. MINI-MAGNET Feet for RGB LED Matric (optional)
+- 5 pcs. 16 Pins Connector Flat Ribbon Cable Female Connector Length 30cm 2.54mm Pitch (optional)
+- 2 pcs. 6"x18" steel sheets (optional)
+- 1 pc. SDCard with the latest RaspiOS on it 
+
+I do not have any wood/metalworking abilities or facilities, so the optional items were used to mount the LED panels without any kind of a custom frame. 
+
+## Software
+
+Create a directory on your RPi and clone this repository into it
+
+	
+#### Misc
+- An account and an AppId from [open weather map](openweathermap.org)
+	-  the account is free and is required to obtain the weather information
+- [the latest Raspberry Pi OS image](www.raspberrypi.org/software/operating-systems)
+	- downloading and installation directions are on the website
+#### LED
+- [hzellers rpi-rgb-led-matrix](github.com/hzeller/rpi-rgb-led-matrix) 
+	- installation and usage directions are on the GitHub page. This is an excellant and powerful library for LED Panels.
+
+
+
+## Installation
+Before installing any new package or software, you should make sure your RPi is up to date by running the following commands:
+
+	sudo apt-get update
+    sudo apt-get -y full-upgrade
+   
+#### Git the weatherPanel Application
+
+	git clone <code uri from github>
+    
+this should result in output something like this
+	
+    Cloning into 'wethaerPanel'...
+	remote: Enumerating objects: 111, done.
+	remote: Counting objects: 100% (111/111), done.
+	remote: Compressing objects: 100% (73/73), done.
+	remote: Total 111 (delta 48), reused 93 (delta 34), pack-reused 0
+	Receiving objects: 100% (111/111), 2.07 MiB | 3.36 MiB/s, done.
+	Resolving deltas: 100% (48/48), done.
+and create the directory weatherPanel
+
+#### Git and Install the hzeller Library
+I strongly recommend going to the hzellers github web page and reading through the documentation, it is full of useful information on how to configure and use LED panels in general and with the hzellers library.
+
+	cd ~/ # or where ever you want to place the library
+    git clone git@github.com:hzeller/rpi-rgb-led-matrix.gi
+    cd rpi-rgb-led-matrix/
+	sudo make
+    cd bindings/paython
+   
+Then follow the directions is the *README.md* file to install the bindings for python3
+
+
+#### Configuration
+
+###### Open Weather Map
+if you do not already have an account with Open Weather Map go to 
+
+[Open Weather Map](openweathermap.org)
+
+and create an account, the free one is sufficient, once you have created your account, login and click on your user name in the menu bar. Select **My API keys** and make a note of your key.
+
+###### Configuration File
+
+Using your favorite editor, edit the *sample_weatherPanel.cfg* file in the *weatherPanel* directory, the fields are:
+- **[WEATHER]** section contains the values needed to obtain the weather information for your location
+	- the **lat** and **lon** are the latitude and longitude from which you want the weather
+	- **appid** is the API key you obtained from openweathermap.org
+	- **units** are imperial or metric
+- **[LED]** section contains the flags specified for the hzellers library software, these shouldn't need to be changed, if you need additional flags you can either pass them to the application in the command line or modify the code.
+- **[RSS]** section contains the RSS feeds to be displayed
+	- **RSS** the url of the feed(s) to be shown in the bottom scroll region, if there are more than one RSS feed, then the additional urls must each be on a seperate line and indented as shown in the *sample_weatherPanel.cfg* file
+- **[MISC]** section contains additional settings
+	- **weatherTimeOut** the number of passes through the main loop to make before switching to displaying the RRS feed(s)
+	- **rssTimeOut** the number of passes through the main loop to make before switching to displaying the weather alerts, if there are any
+
+Save the edited file as *weatherPanel.cfg*
+
+###### Additional Libraries
+
+- feedparser - parses the RSS feed
+	- sudo pip3 install feedparser
+
+## And We're Off
+
+That should do it, to run it:
+
+    chmod +x weatherPanel.py #once only
+    sudo ./weatherPanel.py
+
+## Extras
+If you wish to run and stop the application from crontab, there are two shell scripts, *run.sh* and *stop.sh* that can be used to to do that 
+
+#### Assembly
+I don't have the skills or facilities to make a frame to this, so I improvised. I took the two metal sheets and taped them with packing tape to piece of cardboard. I then attached the magnetic feet to the backs of the LED panels (with the extenders to give the wiring more room) and stuck them to the metal sheets. Then using the mounting tabs of your choice you can mount the whole thing on the wall. 
+    
---
+++ weatherPanel.py
@@ -0,0 +1,491 @@
+#!/usr/bin/python3
+from samplebase import SampleBase
+from rgbmatrix import graphics, RGBMatrix, RGBMatrixOptions
+from PIL import Image
+import time
+from datetime import datetime
+import json
+import signal
+import sys
+import requests
+import io
+from os import path
+import configparser
+import feedparser
+import numpy as np
+
+
+class rss:
+    
+    def __init__(self):
+        self.limit = 12 * 3600 * 1000
+        self.current_time_millis = lambda: int(round(time.time() * 1000))
+        self.current_timestamp = self.current_time_millis()
+        self.posts = {} 
+        
+#
+# function to determine if post is in db
+#
+    def post_is_in_db(self, title):
+        if title in self.posts.keys():
+            return True
+        else:
+            return False
+
+# return true if the title is in the database with a timestamp > limit
+    def post_is_in_db_with_old_timestamp(self, title):
+        for key in self.posts.keys():
+            # print("key = '{}', post = '{}'".format(key, self.posts[key]))
+            if title == self.posts[key].title:
+                ts_as_string = str(key)
+                ts = long(ts_as_string)
+                if current_timestamp - ts > limit:
+                    return True
+        return False
+
+
+    def sort_posts(self):
+        #
+        # get the feed data from the url
+        #
+        self.feed = feedparser.parse(self.url)
+        print("got {} entries from {}".format(len(self.feed.entries), self.url))
+
+        #
+        # figure out which posts to print
+        #
+        self.posts_to_print = []
+        self.posts_to_skip = []
+
+        # print("number of entries = {}".format(len(self.feed.entries)))
+        for post in self.feed.entries:
+            # if post is already in the database, skip it
+            # TODO check the time
+            title = post.title
+            # print("checking title '{}'".format(title))
+            if self.post_is_in_db_with_old_timestamp(title):
+                self.posts_to_skip.append(title)
+            else:
+                # print("adding {} to db".format(title))
+                self.posts_to_print.append(title)
+        self.add_posts_to_db()
+
+    def add_posts_to_db(self):
+        #
+        # add all the posts we're going to print to the database with the current timestamp
+        # (but only if they're not already in there)
+        #
+        for title in self.posts_to_print:
+            if not self.post_is_in_db(title):
+                self.posts[str(self.current_timestamp)] = title
+
+    def print_posts(self):
+        #
+        # output all of the new posts
+        #
+        count = 1
+        blockcount = 1
+        # print("posts to print = {}".format(len(self.posts_to_print)))
+        for title in self.posts_to_print:
+            if True:
+                print("\n" + time.strftime("%a, %b %d %I:%M %p") + '  ((( ' + self.feed_name + ' - ' +
+                      str(blockcount) + ' )))')
+                print("-----------------------------------------\n")
+                blockcount = blockcount + 1
+                print(title + "\n")
+                count = count + 1
+
+
+
+class panelApp(SampleBase):
+
+    def handler(self, signum, frame):
+        self.offscreen_canvas.Clear()
+        sys.exit(0)
+
+    """
+    Initialize
+    """
+    def __init__(self, *args, **kwargs):
+        super(panelApp, self).__init__(*args, **kwargs)
+        self.rssApp = rss()
+        signal.signal(signal.SIGINT, self.handler)
+        signal.signal(signal.SIGTERM, self.handler)
+    
+    """
+    get the curent weather data, if it is unavailable then just use what we have
+    from the last successful call
+    """
+    def getWeather(self):
+        print("getting weather from {}?{} at {}".format(self.weatherUri, self.params,
+                                                        datetime.now().strftime("%c")))
+        try:
+            r = requests.get(url=self.weatherUri, params=self.params)
+        except:
+            e = sys.exc_info()[0]
+            print("Exception found while getting weather data: " + str(e))
+            return
+        data = r.json()
+        self.parseData(data)
+        
+    """
+    parse the Json returned from getWeather()
+    """
+    def parseData(self, data):
+        current=data["current"]
+        self.sunrise=current["sunrise"]
+        self.sunset=current["sunset"]
+        self.temperatureNow=current["temp"]
+        self.clouds=current["clouds"]
+        self.windSpeed=current["wind_speed"]
+        self.windDir=self.degToCompass(int(current["wind_deg"]))
+        self.humidity=current["humidity"]
+        # there may not be any alerts currently
+        try:
+            alerts=data["alerts"]
+        except:
+            alerts=[]
+        iconId = current["weather"][0]["icon"]
+        self.loadAndSaveIcon(iconId)
+        filename = "./icons/" + iconId + ".png"
+        self.weatherIcon = Image.open(filename)
+
+        # this will delete any 'old' alerts on every other call
+        # allowing us to alternate between alerts and RSS feed
+        for alert in alerts:
+            if not alert in self.alertArray:
+                self.alertArray.append(alert["description"].replace("\n", " "))
+            else:
+                self.alertArray.remove(alert)
+
+        self.daily = data["daily"]
+        temperatures = self.daily[0]["temp"]
+        self.minTemp = temperatures["min"]
+        self.maxTemp = temperatures["max"]
+
+
+    def degToCompass(self, num):
+        val=int((num/22.5)+.5)
+        arr=["N","NNE","NE","ENE","E","ESE", "SE", "SSE","S","SSW","SW","WSW","W","WNW","NW","NNW"]
+        return arr[(val % 16)]
+    
+
+    """
+    Image handlers
+    """
+    def trimImage(self, image):
+        width, height = image.size
+        image_data = np.asarray(image)
+        image_data_bw = image_data.max(axis=2)
+        non_empty_columns = np.where(image_data_bw.max(axis=0)>0)[0]
+        non_empty_rows = np.where(image_data_bw.max(axis=1)>0)[0]
+        cropBox = (min(non_empty_rows), max(non_empty_rows), min(non_empty_columns),
+                   max(non_empty_columns))
+        image_data_new = image_data[cropBox[0]:cropBox[1]+1, cropBox[2]:cropBox[3]+1 , :]
+        new_image = Image.fromarray(image_data_new)
+        return new_image
+
+
+    """
+    load and save the png to disk iffi it is not already saved
+    """
+    def loadAndSaveIcon(self, iconId):
+        url = "http://openweathermap.org/img/w/" + iconId + ".png"
+        filename = "./icons/" + iconId + ".png"
+        if not path.exists(filename): 
+            r = requests.get(url, allow_redirects=True)
+            imgfile = io.StringIO(r.content)
+            img = Image.open(imgfile)
+            rgba = np.array(img)
+            rgba[rgba[...,-1]==0] = [0, 0, 0, 0]
+            im = Image.fromarray(rgba)
+            im = self.trimImage(im)
+            im.thumbnail((36, 36))
+            im.save(filename)
+        else:
+            im = Image.open(filename)
+        return im
+                
+        
+    def drawImage(self, imageFilename):
+        image=Image.open(imageFilename)
+        image.thumbnail((self.iconSize, self.iconSize), Image.ANTIALIAS)
+        self.matrix.SetImage(image.convert('RGB'), self.xpos, 30)
+        
+
+
+    def drawScreen(self):
+        pos = 0
+        alertNo = 0
+        rssNo = 0
+        lineHeight = 10
+
+        data = self.getWeather()
+        self.rssApp.sort_posts()
+
+        ypos = self.offscreen_canvas.height - self.lineSpacing
+        self.xpos=self.offscreen_canvas.width
+
+        loopCounter = 0
+        timeOut = self.rssTimeOut
+        # timeOut = 8 * 60 * 2 # debugging only
+        tensec = 8 * 10 
+        
+        dayIndex = 0
+        dayName = "Today"
+        
+        # initialize the weather info iffi the http request has completed
+        #
+        if not self.weatherIcon == None:
+            temperatures = self.daily[0]["temp"]
+            minTemp = temperatures["min"]
+            maxTemp = temperatures["max"]
+            iconId = self.daily[0]["weather"][0]["icon"]
+            self.loadAndSaveIcon(iconId)
+            filename = "./icons/" + iconId + ".png"
+            weatherIcon = Image.open(filename)
+            feedSwitch = False
+
+        while True:
+            # use this to time the loop and set the intervals for getWeather() and dayIndex
+            # on a Pi Zero W it's 8 loops/sec
+            # print("loopCounter = {} at {}".format(loopCounter, datetime.now().strftime("%c")))
+            now = datetime.now()
+            timeNow = now.strftime("%I:%M")
+            dateNow = now.strftime("%a %b %-d %Y")
+            self.offscreen_canvas.Clear()
+
+            # init the weather info iffi the http request has completed
+            #
+            if not self.weatherIcon == None:
+                # increment the dayIndex every minute
+                if loopCounter % tensec == 0:
+                    dayIndex = (dayIndex + 1) % 7
+                    if dayIndex == 0:
+                        dayName = "Today"
+                    else:
+                        dayName = time.strftime("%a", time.localtime(self.daily[dayIndex]["dt"]))
+                    temperatures = self.daily[dayIndex]["temp"]
+                    minTemp = temperatures["min"]
+                    maxTemp = temperatures["max"]
+                    iconId = self.daily[dayIndex]["weather"][0]["icon"]
+                    weatherIcon = self.loadAndSaveIcon(iconId)
+                
+            # draw the date
+            slen1 = graphics.DrawText(self.offscreen_canvas, self.font,
+                                    12, lineHeight, graphics.Color(255,215, 0),
+                                      dateNow)
+            # draw the time
+            slen2 = graphics.DrawText(self.offscreen_canvas, self.fontB,
+                                    42, 2 * lineHeight + 1,
+                                      graphics.Color(218, 32, 32), timeNow)
+            # draw the alert text or the RSS feed if there are no alerts
+            # if feedSwitch is True - draw alerts, else draw RSS
+            if feedSwitch:
+                if len(self.alertArray) > 0:
+                    slen3 = graphics.DrawText(self.offscreen_canvas, self.fontMed,
+                                              self.xpos, ypos, graphics.Color(255, 20, 20),
+                                              self.alertArray[alertNo])
+            else:
+                # print("printing post[{}] = '{}'".format(rssNo, self.rssApp.posts_to_print[rssNo]))
+                if len(self.rssApp.posts_to_print) > 0:
+                    slen3 = graphics.DrawText(self.offscreen_canvas, self.fontMed,
+                                              self.xpos, ypos, graphics.Color(255, 20, 20),
+                                              self.rssApp.posts_to_print[rssNo])
+                
+            # draw the weather info iffi the http request has completed
+            #
+            if not self.weatherIcon == None:
+                self.offscreen_canvas.SetImage(weatherIcon.convert('RGB'), 42,
+                                               2*lineHeight + self.lineSpacing + 1)
+
+                # draw the label
+                slen = graphics.DrawText(self.offscreen_canvas, self.fontSmall,
+                                         2, 2*lineHeight+self.lineSpacing, graphics.Color(255, 255, 255),
+                                         "Now:")
+                
+                # draw the current temperature
+                slen4 = graphics.DrawText(self.offscreen_canvas, self.fontMed,
+                                          2, 3*lineHeight+self.lineSpacing, graphics.Color(0, 255, 0),
+                                          str(int(self.temperatureNow)) + u'\u00b0'+"F")
+                # draw the hunidity
+                slen4 = graphics.DrawText(self.offscreen_canvas, self.fontMed,
+                                          2, 4*lineHeight+self.lineSpacing, graphics.Color(0, 128, 255),
+                                          str(int(self.humidity)) + "%")
+                # draw the wind speed and direction
+                slen4 = graphics.DrawText(self.offscreen_canvas, self.fontMed,
+                                          12, 5*lineHeight + 2, graphics.Color(0, 255, 255),
+                                          str(int(self.windSpeed)) + "mph from the " + self.windDir)
+                
+                
+                # draw the label
+                slen = graphics.DrawText(self.offscreen_canvas, self.fontSmall,
+                                         50+46, 2*lineHeight+self.lineSpacing, graphics.Color(255, 255, 255),
+                                         dayName + ":")
+                
+                # draw the min/max temperatures
+                slen5 = graphics.DrawText(self.offscreen_canvas, self.fontMed,
+                                          50+36, 3*lineHeight+self.lineSpacing,
+                                          graphics.Color(0, 255, 128),
+                                          str(int(minTemp)) + "/" +
+                                          str(int(maxTemp)) + u'\u00b0'+"F")
+
+            # move the scroll text 1 pixel
+            self.xpos = self.xpos - 1;
+            
+            if len(self.alertArray) > 0 or len(self.rssApp.posts_to_print) > 0:
+                if (self.xpos + slen3) < 0:
+                    # since fetching the weather/RSS feed can change
+                    # the scrolling text, only do it when there is
+                    # nothing to scroll
+
+                    # get the weather and RSS info every timeOut passes through the loop
+                    # if(loopCounter % 20) == 0:
+                        # print("loopCounter = {}, timeOut = {}".format(loopCounter, timeOut))
+                    if loopCounter > timeOut:
+                        # change the scrolling display data
+                        feedSwitch = not feedSwitch
+                        if feedSwitch:
+                            data = self.getWeather()
+                            timeOut = self.weatherTimeOut
+                        else:
+                            timeOut = self.rssTimeOut
+                            # if there is more than 1 feed then rotate feeds
+                            feedArrayLen = len(self.feedArray)
+                            if feedArrayLen > 1:
+                                self.feedNo += 1
+                            if self.feedNo >= feedArrayLen:
+                                self.feedNo = 0
+                            self.rssApp.url = self.feedArray[self.feedNo] 
+                            self.rssApp.sort_posts()
+
+                        #reset the loop counter
+                        loopCounter = 0
+
+
+                    # Start scrolling the next alert or RSS entry
+                    self.xpos = self.offscreen_canvas.width 
+                    if feedSwitch:
+                        alertNo = alertNo + 1
+                        if alertNo >= len(self.alertArray):
+                            alertNo = 0
+                    else:
+                        rssNo = rssNo + 1
+                        # print("rssNo = {}, length = {}".format(rssNo, len(self.rssApp.posts_to_print)))
+                        if rssNo >= len(self.rssApp.posts_to_print):
+                            rssNo = 0
+                
+            time.sleep(0.05)
+            self.offscreen_canvas = self.matrix.SwapOnVSync(self.offscreen_canvas)
+            loopCounter = loopCounter + 1
+
+    """
+    error handler for options, if second arg is '', the the whole section is missing
+    or invalid
+    """
+    def handleOptionsError(self, section, key):
+        if key == '':
+            print("The options section '"+section+"' is missing or invalid")
+        else:
+            print("The key '"+key+"' in section '"+section+"' is missing or invalid")
+        self.offscreen_canvas.Clear()
+        sys.exit(-1)
+
+
+    def setup(self):
+        config = configparser.ConfigParser()
+        config.read('weatherPanel.cfg')
+        
+        #
+        # LED section
+        #
+        options = RGBMatrixOptions()
+        if 'LED' in config:
+            ledOptions = config['LED']
+            options.cols = int(ledOptions.get('cols', 64))
+            options.rows = int(ledOptions.get('rows', 32))
+            options.chain_length = int(ledOptions.get('chain_length', 4))
+            options.parallel = int(ledOptions.get('parallel', 1))
+            options.hardware_mapping = ledOptions.get('hardware_mapping', 'adafruit-hat')
+            options.pixel_mapper_config = ledOptions.get('pixel_mapper_config', 'U-mapper')
+            self.matrix = RGBMatrix(options = options)
+        else:
+            self.handleOptionsError('LED', '')
+
+        #
+        # WEATHER section
+        #
+        if 'WEATHER' in config:
+            weatherOptions = config['WEATHER']
+            if 'lat' in weatherOptions:
+                lat = weatherOptions.get('lat')
+            else:
+                self.handleOptionsError('WEATHER', 'lat')
+            if 'lon' in weatherOptions:
+                lon = weatherOptions.get('lon')
+            else:
+                self.handleOptionsError('WEATHER', 'lon')
+            if 'units' in weatherOptions:
+                units = weatherOptions.get('units', 'imperial')
+            if 'appid' in weatherOptions:
+                appid = weatherOptions['appid']
+            else:
+                self.handleOptionsError('WEATHER', 'appid')
+        else:
+            self.handleOptionsError('WEATHER', '')
+        self.weatherUri="https://api.openweathermap.org/data/2.5/onecall"
+        self.params="lat="+lat+"&lon="+lon+"&appid="+appid+"&units="+units
+
+
+        #
+        # RSS section
+        #
+        self.feedArray = []
+        if 'RSS' in config:
+            rssOptions = config['RSS']
+            if 'feed' in rssOptions:
+                self.feedArray = rssOptions.get('feed', '').split("\n")
+                self.rssApp.url = self.feedArray[0]
+
+        #
+        # MISC section
+        #
+        if 'MISC' in config:
+            miscOptions = config['MISC']
+            self.weatherTimeOut = int(miscOptions.get('weatherTimeOut'))
+            self.rssTimeOut = int(miscOptions.get('rssTimeOut'))
+        else:
+            self.weatherTimeOut =  8 * 60 * 10
+            self.rssTimeOut =  8 * 60 * 30
+            
+        self.offscreen_canvas = self.matrix.CreateFrameCanvas()
+        self.font = graphics.Font()
+        self.font.LoadFont("./fonts/7x13.bdf")
+        self.fontB = graphics.Font()
+        self.fontB.LoadFont("./fonts/7x13B.bdf")
+        self.fontMed = graphics.Font()
+        self.fontMed.LoadFont("./fonts/6x9.bdf")
+        self.fontSmall = graphics.Font()
+        self.fontSmall.LoadFont("./fonts/5x8.bdf")
+        self.textColor = graphics.Color(0, 0, 128)
+        self.xpos = 0
+        self.lineSpacing = 1
+    
+        self.degreeSign = '\u00B0'
+        
+        self.alertArray = []
+        self.weatherIcon = None
+        self.feedNo = 0
+
+def main():
+    app = panelApp()
+    app.setup()
+    app.drawScreen()
+
+        
+# Main function
+if __name__ == "__main__":
+    main()
+    
+        
+
